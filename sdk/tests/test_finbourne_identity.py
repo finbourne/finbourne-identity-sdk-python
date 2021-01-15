face9014ccede2b922de1f736242864b45d7c718
Stephen LM <engineering@finbourne.com>
2021-01-15 11:54:18 +0000

    SE-696: checkpoint

--- sdk/tests/test_finbourne_identity.py
+++ sdk/tests/test_finbourne_identity.py
@@ -2,15 +2,17 @@ import unittest
 from finbourne_identity import api as la, ApiClientBuilder
 from finbourne_identity.utilities import ApiClientFactory
 
-api_client = ApiClientFactory()
-domains_api = api_client.build(la.DomainsApi)
 
 class Lydia(unittest.TestCase):
 
-    def test_policies(self):
-        domain = domains_api.get_my_domain()
-        self.assertIsNotNone(domain)
+    @classmethod
+    def setUpClass(cls):
+        api_client = ApiClientFactory()
+        cls.domains_api = api_client.build(la.DomainsApi)
 
+    def test_policies(self):
+        domain = self.domains_api.get_my_domain()
+        self.assertIsNotNone(domain)
 
 if __name__ == '__main__':
     unittest.main()
