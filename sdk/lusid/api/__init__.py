03c63f2fd844db5b58d2fe97293f4fae3b93fc51
Stephen LM <engineering@finbourne.com>
2021-01-18 21:03:28 +0000

    SE-696: checkpoint

---
+++ sdk/lusid/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lusid.api.application_metadata_api import ApplicationMetadataApi
+from lusid.api.applications_api import ApplicationsApi
+from lusid.api.authentication_api import AuthenticationApi
+from lusid.api.domains_api import DomainsApi
+from lusid.api.personal_authentication_tokens_api import PersonalAuthenticationTokensApi
+from lusid.api.roles_api import RolesApi
+from lusid.api.tokens_api import TokensApi
+from lusid.api.users_api import UsersApi
