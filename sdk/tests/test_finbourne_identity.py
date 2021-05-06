7a88ef0cd8f09c967e25fff46659069afe558fdf
Omar-Oz <omar.osman@finbourne.com>
2021-05-06 10:25:55 +0100

    remove lusid import

--- sdk/tests/test_finbourne_identity.py
+++ sdk/tests/test_finbourne_identity.py
@@ -2,14 +2,13 @@ import unittest
 from finbourne_identity import ApiClientBuilder as identity_api
 from finbourne_identity import api as iapi
 from lusidjam import RefreshingToken
-import lusid
-
+from finbourne_identity.utilities import ApiClientFactory
 
 class Lydia(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        lusid_api_factory = lusid.utilities.ApiClientFactory(token=RefreshingToken(),
+        lusid_api_factory = ApiClientFactory(token=RefreshingToken(),
                                                              api_secrets_filename="secrets.json")
         api_client = lusid_api_factory.api_client
         identity_api_client = identity_api.build(api_secrets_filename="secrets.json",
