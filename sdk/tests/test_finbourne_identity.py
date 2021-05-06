2bbe2ce4d3def521bfc6797f0bf915fe4c278596
Omar-Oz <omar.osman@finbourne.com>
2021-05-06 10:00:37 +0100

    SE-1036:Update SDK tests

--- sdk/tests/test_finbourne_identity.py
+++ sdk/tests/test_finbourne_identity.py
@@ -1,18 +1,30 @@
 import unittest
-from finbourne_identity import api as la, ApiClientBuilder
-from finbourne_identity.utilities import ApiClientFactory
+from finbourne_identity import ApiClientBuilder as identity_api
+from finbourne_identity import api as iapi
+from lusidjam import RefreshingToken
+import lusid
 
 
 class Lydia(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        api_client = ApiClientFactory()
-        cls.domains_api = api_client.build(la.DomainsApi)
+        lusid_api_factory= lusid.utilities.ApiClientFactory(token=RefreshingToken(),api_secrets_filename="secrets.json")
+        api_client = lusid_api_factory.api_client
+        identity_api_client = identity_api.build(api_secrets_filename="secrets.json", token=api_client.configuration.access_token)
+        cls.users_api = iapi.UsersApi(identity_api_client)
+        cls.roles_iapi = iapi.RolesApi(identity_api_client)
 
-    def test_policies(self):
-        domain = self.domains_api.get_my_domain()
-        self.assertIsNotNone(domain)
+    def test_roles(self):
+        identity_roles = self.roles_iapi.list_roles()
+        self.assertIsNotNone(identity_roles)
+        self.assertEqual(type(identity_roles[0].to_dict()['description']),str)
+        self.assertIsNot(identity_roles, str)
+
+    def test_users(self):
+        user_list = self.users_api.list_users()
+        self.assertIsNotNone(user_list[0].to_dict()['id'])
+
 
 if __name__ == '__main__':
     unittest.main()
