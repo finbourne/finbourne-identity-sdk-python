4dfd8d25ee09fc35c19a5568de2b025c49dba36f
Omar-Oz <omar.osman@finbourne.com>
2021-05-06 10:09:06 +0100

    fix code format

--- sdk/tests/test_finbourne_identity.py
+++ sdk/tests/test_finbourne_identity.py
@@ -9,16 +9,18 @@ class Lydia(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        lusid_api_factory= lusid.utilities.ApiClientFactory(token=RefreshingToken(),api_secrets_filename="secrets.json")
+        lusid_api_factory = lusid.utilities.ApiClientFactory(token=RefreshingToken(),
+                                                             api_secrets_filename="secrets.json")
         api_client = lusid_api_factory.api_client
-        identity_api_client = identity_api.build(api_secrets_filename="secrets.json", token=api_client.configuration.access_token)
+        identity_api_client = identity_api.build(api_secrets_filename="secrets.json",
+                                                 token=api_client.configuration.access_token)
         cls.users_api = iapi.UsersApi(identity_api_client)
         cls.roles_iapi = iapi.RolesApi(identity_api_client)
 
     def test_roles(self):
         identity_roles = self.roles_iapi.list_roles()
         self.assertIsNotNone(identity_roles)
-        self.assertEqual(type(identity_roles[0].to_dict()['description']),str)
+        self.assertEqual(type(identity_roles[0].to_dict()['description']), str)
         self.assertIsNot(identity_roles, str)
 
     def test_users(self):
