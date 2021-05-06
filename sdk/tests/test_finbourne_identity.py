4a1b8ce9b77f689d5b3c2b76b308dad59c8b2f6e
Omar-Oz <81164562+Omar-Oz@users.noreply.github.com>
2021-05-06 12:03:03 +0100

    SE-1036:Update SDK tests for pipeline
    * SE-1036:Update SDK tests
    
    * fix code format
    
    * remove lusid import
    
    * Remove LusidJam Reference
    
    * Remove desc test
    
    * Final Fix

--- sdk/tests/test_finbourne_identity.py
+++ sdk/tests/test_finbourne_identity.py
@@ -1,18 +1,24 @@
 import unittest
-from finbourne_identity import api as la, ApiClientBuilder
+from finbourne_identity import api as iapi
 from finbourne_identity.utilities import ApiClientFactory
 
-
-class Lydia(unittest.TestCase):
+class IdentityTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        api_client = ApiClientFactory()
-        cls.domains_api = api_client.build(la.DomainsApi)
+        api_client_factory = ApiClientFactory(api_secrets_filename="secrets.json")
+        cls.users_api = api_client_factory.build(iapi.UsersApi)
+        cls.roles_iapi = api_client_factory.build(iapi.RolesApi)
 
-    def test_policies(self):
-        domain = self.domains_api.get_my_domain()
-        self.assertIsNotNone(domain)
+    def test_roles(self):
+        identity_roles = self.roles_iapi.list_roles()
+        self.assertIsNotNone(identity_roles)
+        self.assertIsNot(identity_roles, str)
+
+    def test_users(self):
+        user_list = self.users_api.list_users()
+        self.assertGreater(len(user_list), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
