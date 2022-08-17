7303723ceb347503140fbbf1c41e8ff63c6fb769
Paul Saunders <paul.saunders@finbourne.com>
2022-08-17 12:39:58 +0100

    CTECH-1723: Fixes import.
    Signed-off-by: Paul Saunders <paul.saunders@finbourne.com>

--- sdk/tests/utilities/test_retry.py
+++ sdk/tests/utilities/test_retry.py
@@ -5,7 +5,7 @@ import finbourne_identity.api
 from finbourne_identity import ApiException
 from finbourne_identity.utilities import ApiClientFactory
 
-from utilities import CredentialsSource
+from . import CredentialsSource
 
 
 class MockApi:
