b259f79ba15072c616acb9f9d86039f4bcdf5ee4
Adam Massoud <adam.massoud@finbourne.com>
2023-01-18 12:35:41 +0000

    CTECH-2239: Add config_keys class so works with fbnsdkutilities

---
+++ sdk/tests/utilities/test_config_keys.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from finbourne_identity.utilities.config_keys import ConfigKeys
+
+
+class TestConfigKeys(TestCase):
+    def test_identity_url_in_config_keys(self):
+        # Arrange
+        expected_config_keys_subset = {
+            "api_url": {"env": "FBN_LUSID_IDENTITY_URL", "config": "identityUrl"}
+        }
+
+        # Act
+        actual_config_keys = ConfigKeys().get()
+
+        # Assert
+        self.assertDictContainsSubset(expected_config_keys_subset, actual_config_keys)
