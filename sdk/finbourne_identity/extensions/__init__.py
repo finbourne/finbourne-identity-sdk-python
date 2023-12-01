227675230cc76795dae1c1230a32d8b1310165c5
Concourse <concourse@finbourne.com>
2023-12-01 11:41:18 +0000

    Updates for v2.0.1

--- sdk/finbourne_identity/extensions/__init__.py
+++ sdk/finbourne_identity/extensions/__init__.py
@@ -1,15 +1,7 @@
-from finbourne_identity.extensions.api_client_builder import build_client
 from finbourne_identity.extensions.api_client_factory import SyncApiClientFactory, ApiClientFactory
 from finbourne_identity.extensions.configuration_loaders import (
     ConfigurationLoader,
     SecretsFileConfigurationLoader,
     EnvironmentVariablesConfigurationLoader,
     ArgsConfigurationLoader,
-    get_api_configuration,
 )
-from finbourne_identity.extensions.api_configuration import ApiConfiguration
-from finbourne_identity.extensions.retry import RetryingRestWrapper, RetryingRestWrapperAsync
-from finbourne_identity.extensions.proxy_config import ProxyConfig
-from finbourne_identity.extensions.refreshing_token import RefreshingToken
-from finbourne_identity.extensions.api_client import SyncApiClient
-from finbourne_identity.extensions.rest import RESTClientObject
